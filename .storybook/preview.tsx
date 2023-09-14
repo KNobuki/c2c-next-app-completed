b8160585f65309300b5c2a58895e182b053aef9f
CodeSandbox <support@codesandbox.io>
2023-09-14 15:47:23 +0000

    Sandbox snapshot commit

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Preview } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../src/themes";
+import * as NextImage from "next/image";
 
 const preview: Preview = {
   decorators: [
@@ -12,5 +13,17 @@ const preview: Preview = {
     ),
   ],
 };
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) =>
+    typeof props.src === "string" ? (
+      <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+    ) : (
+      <OriginalNextImage {...props} unoptimized />
+    ),
+});
 
 export default preview;
