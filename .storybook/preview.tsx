e3803dd7c684ec845e6095823fc8f341e8eaf8fe
CodeSandbox <support@codesandbox.io>
2023-09-14 14:52:37 +0000

    Sandbox snapshot commit

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,35 +1,13 @@
 import React from "react";
 import { Preview } from "@storybook/react";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { theme } from "../src/themes";
-import * as NextImage from "next/image";
-
-export const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  textarea {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  a {
-    text-decoration: none;
-    transition: .25s;
-    color: #000000;
-  }
-`;
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-        {/* <GlobalStyle> */}
         <Story />
-        {/* </GlobalStyle> */}
       </ThemeProvider>
     ),
   ],
