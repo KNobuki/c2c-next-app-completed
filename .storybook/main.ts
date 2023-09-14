a2116a9a5a6f40899b4be499826b0036646e6627
CodeSandbox <support@codesandbox.io>
2023-09-14 14:57:46 +0000

    Sandbox snapshot commit

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -16,5 +19,21 @@ const config: StorybookConfig = {
     autodocs: "tag",
   },
   staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-propperties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-property-in-object",
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ];
+    return config;
+  },
 };
 export default config;
